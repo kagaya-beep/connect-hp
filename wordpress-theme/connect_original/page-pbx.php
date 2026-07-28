c0b9f010b1a2ec91c5bae96c521e114134730f85
hayashi-beep <hayashi@connect-web.jp>
2026-07-28 15:59:36 +0900

    Update PBX page installation count

---
+++ wordpress-theme/connect_original/page-pbx.php
@@ -0,0 +1,352 @@
+<?php get_header(); ?>
+
+
+<section>
+<div class="h2_container">
+	<h2>クラウドPBX（クラウド型ビジネスフォン）</h2>
+</div>
+</section>
+
+<main>
+
+<section>
+<div class="wrap_1200 pt40 clearfix">
+
+<?php get_sidebar('service'); ?>　
+
+
+<section>
+<div class="main_container02" id="pbx">
+<section>
+<h3 class="sp"><img src="<?php bloginfo('template_directory'); ?>/images/pbx/tit_sp.png" alt="クラウドPBX｜「働く」がもっと柔軟になるクラウド型ビジネスフォン"></h3>
+<h3 class="pc t_c"><img src="<?php bloginfo('template_directory'); ?>/images/pbx/tit.png" alt="クラウドPBX｜「働く」がもっと柔軟になるクラウド型ビジネスフォン"></h3>
+</section>
+
+<section>
+<div class='arrow_box'><p>クラウドで「どこでも」内線・外線</p></div>
+<div class="main_text t_c pb10">
+クラウド型ビジネスフォン（クラウドPBX）とは、<br class="pc">
+電話回線の会社に設置されている交換機（PBX）をクラウド化するサービスです。<br class="pc">
+スマートフォンやパソコンなどのさまざまな端末で、</div>
+<div class="main_catch t_c mb30">
+<span class="t_red">「いつでも」「どこでも」</span>固定電話番号の発信と着信ができ、<br class="pc">
+まるでオフィスにいるかのような 内線取次が可能です。</div>
+<div class="t_c"><img src="https://connect-web.jp/wp-content/themes/connect_original/images/pbx/01.png" alt=""></div>
+</section>
+<section>
+<div class='arrow_box'><p>こんな困りごと、ありませんか？</p></div>
+<div class="cheak_list_box pt60">
+<ul class="cheak_list">
+<li><i class="fas fa-check-square t_blue"></i> 異なる拠点間で内線化できたらいいのに…</li>
+<li><i class="fas fa-check-square t_blue"></i> ビジネスフォンやPBXの導入コストが高すぎる…</li>
+<li><i class="fas fa-check-square t_blue"></i> 転送サービスで通話料が二重になってる…</li>
+<li><i class="fas fa-check-square t_blue"></i> 外出の際に個人の携帯番号を教えたくない…</li>
+<li><i class="fas fa-check-square t_blue"></i> フリーダイヤルを簡単に導入したいけど…</li>
+</ul>
+</div>
+<div class="t_c"><img src="https://connect-web.jp/wp-content/themes/connect_original/images/pbx/02.png" alt=""></div>
+</section>
+
+<section>
+<div class="solution_box">
+<div class='arrow_box'><p>その困りごと、解決できます！</p></div>
+<div class="cheak_list_box pt60">
+<ul class="cheak_list">
+<li><i class="fas fa-check-square t_red"></i> 異なる拠点を内線化して電話番号を共有！電話業務効率の向上！</li>
+
+<li><i class="fas fa-check-square t_red"></i> 転送じゃない！会社の代表番号でどこでも発着信が可能！</li>
+<li><i class="fas fa-check-square t_red"></i> どこから発信しても会社と同じ番号なので<br class="sp">  個人の携帯番号教える必要なし</li>
+<li><i class="fas fa-check-square t_red"></i> 全国局番対応。フリーダイヤル（0120/0800）でも発着信！</li>
+</ul>
+</div>
+
+</div>
+<div class="solution_img"><img src="https://connect-web.jp/wp-content/themes/connect_original/images/pbx/03.png" alt=""></div>
+</section>
+
+<section id="sec_01">
+  <div class='arrow_box'><h4>導入実績33,000社以上！</h4></div>
+        <div class="container">
+          <div style="text-align:center;margin-bottom:20px;"><img src="img/aug23_index_img/mottel_image_banner.jpg" alt="クラウドPBX MOT/TEL"></div>
+            <h3>クラウドPBX「MOT/TEL」は、<br class="sp_none">
+                スマートフォンから会社番号や内線番号で発信できる<br class="sp_none">
+                法人向けクラウドPBXサービスです。</h3>
+            <p class="greeting_comm">アプリをスマホ（iOS・android）にインストールするだけでクラウド電話として利用できるので、<br class="sp_none">
+                従来の会社電話（ビジネスフォン）では必要だった「電話設備・機器」の設置工事やメンテナンスが不要になります。</p>
+
+            <ul class="row">
+                <li class="col-md-6 col-lg-4">
+                    <div class="panel_wrap">
+                        <figure><img src="img/aug23_index_img/sec01_001.png" alt="会社番号での受発信">
+                            <figcaption><span>018</span>などの<br class="sp_none">
+                                会社番号で受発信</figcaption>
+                        </figure>
+                    </div>
+                </li>
+                <li class="col-md-6 col-lg-4">
+                    <div class="panel_wrap">
+                        <figure><img src="img/aug23_index_img/sec01_002_b.png" alt="工事不要">
+                            <figcaption><span>050</span>なら<span>工事</span>不要<br>
+                                <span>設備設置</span>不要</figcaption>
+                        </figure>
+                    </div>
+                </li>
+                <li class="col-md-6 col-lg-4">
+                    <div class="panel_wrap">
+                        <figure><img src="img/aug23_index_img/sec01_003_b.png" alt="メンテナンス不要">
+                            <figcaption><span>故障対応</span>の心配無用<br>
+                                <span>メンテナンス</span>不要</figcaption>
+                        </figure>
+                    </div>
+                </li>
+            </ul>
+            <!--<p class="sec_catch">東北(青森、岩手、宮城、秋田、山形、福島)ではクラウドPBX「 MOT/TEL（モッテル） 」で起業する方が増えています！</p>-->
+        </div>
+        <div class="container margin_t50">
+          <div class='arrow_box'><h4>サービス開始より利用社数連続純増中！</h4></div>
+          <p>
+            「クラウドPBX MOT/TEL（モッテル）」は、アプリ/ソフトやGateway・データセンターのPBXなど全て自社開発。<br class="sp_none">
+使いやすさとマニュアルなどサポートも充実しているため、他社サービスから乗り換えによる利用者様も増加しています。<br class="sp_none">
+また、長年の研究開発の成果である品質も評価されサービス開始以来、7年連続で利用社数が純増中。<br class="sp_none">
+MOTシリーズの累計導入数も33,000社を超えています。</p>
+          <div style="text-align:center;margin-bottom:20px;"><img src="img/aug23_index_img/mottel_junzou_graph.webp" alt="7年連続で利用社数が純増中"></div>
+
+        </div>
+    </section>
+
+<section id="reason" class="margin_t50">
+<div class='arrow_box'><p>選ばれる4つの理由</p></div>
+<h4 class="t_blue"><span>｜</span>1<span>｜</span>会社の電話がいつでもどこでも使える</h4>
+<div class="main_text margin_t25">外出先でも自宅でも、スマホで会社の電話を受け取ることができるので、わざわざ転送サービスに加入する必要はありません。お客様からの連絡を取り逃す心配はありませんし、会社の電話番号での発信もできるので、個人の携帯番号を教える必要がありません。オフィスで受けた電話をスマホへ保留/転送できるので、担当者がどこにいても内線で呼び出しそのままつなぐことができます。部門別など複数の電話番号がある場合にも、電話番号の切り替えが可能です。急なテレワークでも、固定電話機からスマホでの利用への切り替えは、Webで簡単に設定が可能です。</div>
+<div class="mb60 t_c margin_t25"><img src="https://connect-web.jp/wp-content/themes/connect_original/images/pbx/04.png" alt=""></div>
+<h4 class="t_blue margin_t50"><span>｜</span>２<span>｜</span>圧倒的低価格！月額サービスで無駄なコストを低減</h4>
+<div class="main_text margin_t25">初月費用無料で導入時に高額なコストがかかりません。スマホでのご利用であれば、電話機増設に伴う工事費はもちろん移転やレイアウト変更にかかる費用が不要。月額サービスだから従業員の増減に合わせて月額精算が可能なので柔軟な対応ができます。
+個人のスマホでも利用可能なので、社員への携帯端末支給によるコストもかかりません。営業所などの支店間はもちろん、社員同士もスマホで内線通話ができるので、通話料も削減可能。コストを低減することが可能です。</div>
+<div class="mb60 t_c margin_t25"><img src="https://connect-web.jp/wp-content/themes/connect_original/images/pbx/05.png" alt=""></div>
+<div id="sec_rate_plan">
+  <div class="popular_posts_tit">MOT/TELは、月額制のサブスクサービスです。<br class="sp_none">お申込みから最短3営業日で利用可能。短期での解約も可能です。</div>
+        <h4 class="sub_fnctitle">月額<span>6,980</span>円（税別）/20内線</h4>
+        <ul class="row">
+            <li class="col-lg-6">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>初期費用</th>
+                            <td>29,800円<small>※1</small></td>
+                        </tr>
+                        <tr>
+                            <th>内線通話料</th>
+                            <td>0円</td>
+                        </tr>
+                        <tr>
+                            <th>転送料金</th>
+                            <td>0円</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </li>
+            <li class="col-lg-6">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>ライセンス料</th>
+                            <td>0円</td>
+                        </tr>
+                        <tr>
+                            <th>外線通話料</th>
+                            <td>8円/3分<small>※2</small></td>
+                        </tr>
+                        <tr>
+                            <th>端末代</th>
+                            <td>0円</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </li>
+        </ul>
+        <p><small>※1 地域番号を利用するプランでは別途工事費がかかります。<br />
+            ※2 ご利用回線事業者の料金に準ずる。</small></p>
+        <div class="link_btn_area in_site"><a href="../contact/">料金についてお問合せはこちら&nbsp;&nbsp;<i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
+
+</div>
+
+<h4 class="t_blue margin_t50"><span>｜</span>3<span>｜</span>直観的な操作画面で操作は簡単！</h4>
+<!-- 使いやすいアプリ画面_画像右 -->
+<div class="main_text margin_t25">持ち歩けるビジネスフォンとしてのこだわりは機能だけでなく、スマホアプリのUIもビジネスフォンそのもの。スマホアプリでもあえて外線ボタン（パークボタン）を用意するなど、見た目もビジネスフォンをそのまま採用しているので、操作に戸惑うことなく誰でも簡単に利用可能です。</div>
+    <div class="row margin_t25">
+      <div class="col-lg-6">
+
+        <div class="fnc_box">
+          <h4 class="margin_t25">基本機能</h4>
+          <ul class="mb20">
+            <li>内線通話</li>
+            <li>会社番号発信</li>
+            <li>会社番号着信</li>
+            <li>保留</li>
+            <li>転送</li>
+            <li>WEB電話帳</li>
+            <li>就業時間外拒否</li>
+            <li>発着信履歴</li>
+          </ul>
+          <br>
+          <h4 class="margin_t25">連動機能</h4>
+          <ul class="option_func">
+            <li>チャット</li>
+            <li>FAX</li>
+            <li>勤怠管理</li>
+            <li>音声案内</li>
+            <li>通話録音</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div id="photo"> <img src="img/aug23_index_img/cloud-pbx-mobile-interface.webp" alt="クラウドPBX スマホ画面"></div>
+        <!-- viewer -->
+      </div>
+    </div>
+    <!-- /row -->
+<!-- <div class="mb60 t_c"><img src="https://connect-web.jp/wp-content/themes/connect_original/images/pbx/06.png" alt=""></div> -->
+    <!--***************************************** 「業界最安値級の料金プラン」 -->
+
+
+
+<h4 class="t_blue margin_t50"><span>｜</span>4<span>｜</span>地域に根付いた秋田の会社なので安心</h4>
+<div class="main_text margin_t25">株式会社コネクトは秋田市の旭南にある地域に根付いたオフィスの悩みに応える専門の会社です。弊社のスタッフが提案から導入までしっかりサポートするので安心できるとお声をいただいております。</div>
+<div class="mb60 t_c margin_t25"><img src="https://connect-web.jp/wp-content/themes/connect_original/images/pbx/07.jpg" alt=""></div>
+</section>
+
+<section id="voice">
+
+<?php
+$args = array(
+'post_type' => 'voice',//カスタム投稿名
+'taxonomy' => 'cat_voice',     //タクソノミー名を指定
+'term' => 'v-pbx',           //タームのスラッグを指定
+
+'posts_per_page' => 3 //表示件数
+);
+
+//ループ開始
+$query = new WP_Query($args);
+if ($query->have_posts()) :
+while ($query->have_posts()) :
+$query->the_post();
+?>
+
+<div class="popular_posts_tit">お客さまの声</div>
+
+<div class="popular_posts_wrap">
+
+<a href="<?php the_permalink(); ?>">
+<div class="voice_tit">
+<?php $text= SCF::get( 'v_industry' );?>
+<?php if(!empty($text)):?>
+<p><span>FOR</span><?php echo nl2br($text); ?></p>
+<?php endif; ?>
+<?php $img = get_post_meta( get_the_ID(), 'v_img', true ); ?>
+<div class="imgbox">
+<?php if(!empty($img)):?>
+<img src="<?php echo wp_get_attachment_url($img) ?>" alt="">
+<?php else:?>
+<img src="<?php bloginfo('template_directory'); ?>/images/voice/alternative.jpg" alt="">
+<?php endif;?>
+<dl>
+<dt><p>CASE</p></dt>
+<dd><?php the_title(); ?></dd>
+</dl>
+</div><!--/.imgbox-->
+</div>
+
+<div class="voice_service">
+<ul><li><?php
+$terms = get_the_terms( get_the_ID(), 'cat_voice' );
+if( ! empty( $terms ) ) {
+    $image = get_term_meta( $terms[0]->term_id, 'v_serviceicon', true );
+    if( ! empty( $image ) ) {
+        echo wp_get_attachment_image( $image, 'full' );
+    }
+}
+?></li></ul>
+<dl><dt>提供サービス</dt><dd><?php
+$terms = get_the_terms($post->ID, 'cat_voice');
+if ( $terms ) {
+echo $terms[0]->name;
+}
+?></dd></dl>
+</div><!--/voice_service-->
+</a>
+
+
+<?php
+endwhile;
+endif;
+wp_reset_postdata();
+?>
+
+</div><!--/popular_posts_wrap-->
+</section>
+
+
+<section>
+<div class="wrap_1000">
+<?php get_template_part( 'temp_contact_box' ); ?>
+</div><!--/.wrap_1000-->
+</section>
+
+
+
+
+
+
+
+
+
+</div>
+</section>
+<div style="clear: both"></div>
+
+<section>
+
+<?php
+	$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 4,//表示する記事数
+'category_name' => 'u-pbx' // 表示したいカテゴリーのスラッグを指定
+
+				);
+    $posts = get_posts( $args );
+        foreach ( $posts as $post ): // ループの開始
+        setup_postdata( $post ); // 記事データの取得
+?>
+<?php
+  // カテゴリーのデータを取得
+  $a = get_the_category();
+foreach($a as $cats){
+$name=$cats->name;
+if($cats->parent==0){$c=$name;}
+}
+?>
+<div class="wrap_1000 top_catch">
+<h2 class="top_tit">お役立ち情報<div class="nexa-b t_blue">USEFUL</div></h2>
+</div>
+
+<div class="news_block">
+
+
+<!--記事ブロック-->
+<dl>
+<dt><div class="news_date">
+<a href="<?php echo get_category_link($cats->term_id); ?>" class="news_cat <?php echo $cats->slug; ?>"><?php echo $cats->cat_name; ?></a><a href="<?php the_permalink(); ?>"><?php echo get_the_date(); ?></div>
+<?php the_title(); ?></a>
+</dt>
+<dd><a href="<?php the_permalink(); ?>"><img src="<?php echo catch_that_image(); ?>" alt="<?php the_title(); ?>"></a></dd>
+</dl>
+<!--/記事ブロック-->
+<?php
+        endforeach; // ループの終了
+        wp_reset_postdata(); // 直前のクエリを復元する
+?>
+</div>
+
+</section>
+<?php get_footer(); ?>
